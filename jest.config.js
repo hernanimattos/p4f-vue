23af39314133a574bd4d11bc3851e30efd7c1a28
Hernani <hernani.luiz@gmail.com>
2020-04-12 22:29:57 -0300

    config jest

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ const path = require('path');
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   rootDir: path.resolve(__dirname, './'),
-  setupFiles: ['<rootDir>/tests/setup.js']
+  setupFiles: ['<rootDir>/tests/setup.js'],
+  testMatch: ['**/__tests__/*.{j,t}s?(x)', '**/src/**/*.spec.{j,t}s?(x)']
 };
